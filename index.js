bf75a899d7f467a99c421564b344992fefe0f816
Fredrik Østrem <frx.apps@gmail.com>
2017-04-30 23:24:08 +0200

    Using co instead of async/await for backwards compatability

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const path = require('path');
 const fs = require('mz/fs');
 const _ = require('underscore');
+const co = require('co');
 
 const def = (value, defaultValue) => typeof value !== 'undefined' ? value : defaultValue;
 
@@ -22,21 +23,21 @@ module.exports = function(options) {
 
   const cachedTemplates  = new Map();
 
-  async function compileTemplate(name) {
+  const compileTemplate = co.wrap(function* (name) {
     if(cache && cachedTemplates.has(name))
       return cachedTemplates.get(name);
 
     const filename = path.resolve(root, name + extension);
-    const contents = await fs.readFile(filename, 'utf8');
+    const contents = yield fs.readFile(filename, 'utf8');
     const func = _.template(contents, templateSettings);
 
     if(cache)
       cachedTemplates.set(name, func);
 
     return func;
-  }
+  });
 
-  async function render(name, params, options) {
+  const render = co.wrap(function* (name, params, options) {
     const ctx = this;
     if(options == null)
       options = { };
@@ -48,22 +49,22 @@ module.exports = function(options) {
     );
 
     const layoutName = def(options.layout, layout);
-    const templateFunc = await compileTemplate(name);
+    const templateFunc = yield compileTemplate(name);
 
     if(!ctx.type)
       ctx.type = 'html';
     let body = templateFunc(data);
 
     if(layoutName != null) {
-      const layoutFunc = await compileTemplate(layoutName);
+      const layoutFunc = yield compileTemplate(layoutName);
       data.body = body;
       body = layoutFunc(data);
     }
 
     ctx.body = body;
-  }
+  });
 
-  return async function(ctx, next) {
+  return function(ctx, next) {
     ctx.render = render;
     return next();
   };
